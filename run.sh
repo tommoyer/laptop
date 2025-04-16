6baa512d5eb3d2d1df3097a642995538283ecb94
Tom Moyer <tommoyer@gmail.com>
2025-04-16 19:43:10 -0400

    feat: Added EndeavourOS branch. Still needs tested/fixed

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+die() {
+    (($#)) && printf >&2 '%s\n' "$@"
+    usage
+    exit 1
+}
+
+usage() {
+  printf >&2 "$0\n"
+}
+
+export PATH=${PATH}:${HOME}/.local/bin
+
+pkgs=""
+
+if ! which pipx &> /dev/null
+then
+  pkgs+="pipx"
+  echo "Selecting pipx to be installed"
+else
+  echo "pipx already installed"
+fi
+
+if [[ $pkgs != "" ]]
+then
+  sudo pacman -Syu --noconfirm $pkgs
+else
+  echo "All packaages installed"
+fi
+
+if ! pipx list | grep "package ansible" &> /dev/null
+then
+  pipx install --include-deps ansible
+else
+  echo "Ansible already installed"
+fi
+
+if ! ~/.local/bin/ansible-galaxy collection list | grep community.general &> /dev/null
+then
+  ~/.local/bin/ansible-galaxy collection install community.general
+else
+  echo "Skipping installation of community.general"
+fi
+
+# Install any other things from requirements.txt
+ansible-galaxy install -r requirements.yml
+
+~/.local/bin/ansible-playbook playbook.yml -i inventory --ask-become-pass --ask-vault-pass -e "ansible_connection=local"
+
